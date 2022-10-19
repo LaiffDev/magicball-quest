1a329ebefe61722688ec8839dcf819b3105e1fb8
Latiff <106555182+LaiffDev@users.noreply.github.com>
2022-10-19 19:40:04 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+let buttonAnswer = document.getElementById("answer");
+let ballAnswerEl = document.getElementById("ballAnswer");
+
+
+function answer() {
+   let randomNumber = Math.floor(Math.random() * 8);
+   let eightBall = "";
+      switch (randomNumber) {
+            case 0:
+               eightBall = " It is certain";
+            break;
+            case 1:
+               eightBall = "It is decidedly so";
+            break;
+            case 2:
+               eightBall = "Reply hazy try again";
+            break;
+            case 3:
+               eightBall = "Cannot predict now";
+            break;
+            case 4:
+               eightBall = "Do not count on it";
+            break;
+            case 5:
+               eightBall = "My sources say no";
+            break;
+            case 6:
+               eightBall = "Outlook not so good";
+            break;
+            case 7:
+               eightBall = "Signs point to yes";
+               break;
+   }
+   ballAnswerEl.textContent = "The magic ball says : " + eightBall;
+   console.log(eightBall);
+}
+
+answer();
+
+function reset() {
+   ballAnswerEl.textContent = "The magic ball says : ";
+}
+
+reset();
+
+
+
+
+
+
+
+
+
+
